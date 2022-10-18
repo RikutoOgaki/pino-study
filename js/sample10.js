bc7c21bf8a37d5f5cbc668106c591465570ddeb6
Rikuto <orikutobasuke@gmail.com>
2022-10-18 20:12:44 +0900

    sample10.html add

---
+++ js/sample10.js
@@ -0,0 +1,39 @@
+/*
+誰が(要素・タグ)　　なにを(要素の持つプロパティ)　　する(実行・変更)
+p要素のid名txtの文字情報を持つプロパティに「テキスト」を追加したい。
+<p id="txt"></p>
+document.getElementById("txt").innerText
+*/
+
+
+const kak = document.getElementById("tet");
+const lal = document.getElementById("txt");
+document.getElementById("btn").addEventListener('click' ,() => {
+    console.log("クリックした");
+    // document.getElementById("txt").innerText = kak;
+    // Number(kak);
+    if(kak.value>=0&&kak.value<=6){
+        lal.innerText = "子供ですね";
+    }
+    // else if(kak==7<kak&&12>=kak){
+    //     lal.innerText = "小学生ですね";
+    // }
+    // else if(kak ==13,14,15,16,17,18,19){
+    //     lal.innerText = "中高生ですね";
+    // }
+    // else if(kak ==20){
+    //     lal.innerText = "成人してますね";
+    // }
+    // else if(kak ==100){
+    //     lal.innerText = "長生きですね";
+    // }
+
+    // 7~12 小学生
+    // 13~18 中高生ですね
+    // ２０ 成人シてますね
+    // ２１〜９９ 大人ですね
+    // １００ 長生きですね
+    // ０より小さい １３０以上 そんなもんない
+});
+
+
