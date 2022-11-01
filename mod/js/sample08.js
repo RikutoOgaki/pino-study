7ed183fba25ccd20628dbffadae97fa99c9061d1
Rikuto <orikutobasuke@gmail.com>
2022-11-02 02:24:57 +0900

    sample12 add

--- mod/js/sample08.js
+++ mod/js/sample08.js
@@ -1,14 +1,18 @@
 const btn = document.querySelector("#btn");
 const clear = document.getElementById("clear");
 const tet = document.querySelector(".text")
+const eria = document.getElementById("eria");
+const tXet = document.getElementById("tet");
 console.log(tet);
 console.log(btn);
+let text = tXet.value;
 btn.addEventListener('click',()=>{
+    eria.innerHTML = '<div class="text">' + txet + '</div>';
+    console.log(text);
     tet.classList.add("anime");
-    // tet.classList.remove("anime");
+    tet.classList.remove("animetop");
 })
 clear.addEventListener('click', ()=>{
     tet.classList.add("animetop");
     tet.classList.remove("anime");
-    // tet.classList.remove("anime");
 })
