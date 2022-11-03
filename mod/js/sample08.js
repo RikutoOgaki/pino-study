db05befad0fdd49c078422655607dcdd392e6888
Rikuto <orikutobasuke@gmail.com>
2022-11-03 16:13:39 +0900

    sample12 add

--- mod/js/sample08.js
+++ mod/js/sample08.js
@@ -1,17 +1,17 @@
 const btn = document.querySelector("#btn");
 const clear = document.getElementById("clear");
-const tet = document.querySelector(".text")
+const tet = document.querySelector(".anime")
 const eria = document.getElementById("eria");
 const tXet = document.getElementById("tet");
 console.log(tXet);
 console.log(btn);
 // const box = `<div class=text>${tXet.value}</div>`;
 btn.addEventListener('click',()=>{
-    eria.innerHTML = `<div class=text>${tXet.value}</div>`;
-    tet.classList.add("anime");
-    tet.classList.remove("animetop");
+    eria.innerHTML = `<div class=anime>${tXet.value}</div>`;
+    // tet.classList.add("anime");
+    // tet.classList.remove("animetop");
 })
 clear.addEventListener('click', ()=>{
     tet.classList.add("animetop");
-    tet.classList.remove("anime");
+    // tet.classList.remove("anime");
 })
