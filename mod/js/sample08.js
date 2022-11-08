711402419c0702cc797126afefce7c49fa01fbd7
Rikuto <orikutobasuke@gmail.com>
2022-11-08 20:02:37 +0900

    chat add

--- mod/js/sample08.js
+++ mod/js/sample08.js
@@ -1,17 +1,36 @@
-const btn = document.querySelector("#btn");
+const btn = document.getElementById("btn");
 const clear = document.getElementById("clear");
 const tet = document.querySelector(".anime")
 const eria = document.getElementById("eria");
 const tXet = document.getElementById("tet");
 console.log(tXet);
 console.log(btn);
+console.log(clear);
+let tBox;
+// let max = [];
 // const box = `<div class=text>${tXet.value}</div>`;
 btn.addEventListener('click',()=>{
-    eria.innerHTML = `<div class=anime>${tXet.value}</div>`;
+    let ele = document.createElement("div");
+    ele.classList.add("anime");
+    console.log(ele);
+    eria.appendChild(ele);
+    // eria.innerHTML += `<div class="anime">${tXet.value}</div>`;
+    // max.push += `<div class="anime">${tXet.value}</div>`;
+    // console.log(max);
     // tet.classList.add("anime");
     // tet.classList.remove("animetop");
+    // tBox = `<div class=anime>${tXet.value}</div>`
 })
 clear.addEventListener('click', ()=>{
-    tet.classList.add("animetop");
+    // console.log(document.getElementById("anime"));
+    console.log(document.querySelectorAll(".anime"));
+    let chatanime = document.querySelectorAll(".anime");
+    for(let i=0; i<chatanime.length; i++){
+        // document.querySelectorAll(".anime").remove();
+        console.log(chatanime[i]);
+        chatanime[i].remove();
+    }
+    // tet.classList.add("animetop");
+    // tBox.classList.add("animetop");
     // tet.classList.remove("anime");
 })
