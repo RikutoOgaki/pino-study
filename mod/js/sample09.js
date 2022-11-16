4e17bc284771cc270111894a478d55d28e627c3b
Rikuto <orikutobasuke@gmail.com>
2022-11-16 09:35:53 +0900

    modal add

--- mod/js/sample09.js
+++ mod/js/sample09.js
@@ -1,28 +1,29 @@
 const img = document.querySelector("img");
 const main = document.querySelector("main");
-const box = document.getElementById("overlay");
-let mg = box.querySelector("img");
-console.log(mg);
+// const box = document.getElementById("overlay");
+// const mg = box.querySelector("img");
+// console.log(mg);
 console.log(box);
 // console.log(main);
 // console.log(img);
 
 // ↓要素がない状態
-// img.addEventListener('click' ,()=>{
-//     let back = document.createElement("div");
-//     let pic = document.createElement("picture");
-//     let mg = document.createElement("img");
-//     console.log(back);
-//     back.classList.add("overlay");
-//     back.appendChild(pic);
-//     pic.appendChild(mg);
-//     main.appendChild(back);
-// })
+img.addEventListener('click' ,()=>{
+    let back = document.createElement("div");
+    let pic = document.createElement("picture");
+    let mg = document.createElement("img");
+    console.log(back);
+    back.classList.add("overlay");
+    back.appendChild(pic);
+    mg.src = "./images/img_modal_kyouto01_l.jpg"
+    pic.appendChild(mg);
+    main.appendChild(back);
+})
 
 // ↓要素がある状態
-img.addEventListener('click',()=>{
-    box.classList.remove("hiddon");
-})
-box.addEventListener('click', ()=>{
-    box.classList.add("hiddon");
-})
+// img.addEventListener('click',()=>{
+//     box.classList.remove("hiddon");
+// })
+// box.addEventListener('click', ()=>{
+//     box.classList.add("hiddon");
+// })
