5bea6e93d46bb310c379c9dbe022133edc9dce76
Rikuto <orikutobasuke@gmail.com>
2022-11-16 11:02:40 +0900

    mouse add

--- mod/js/sample09.js
+++ mod/js/sample09.js
@@ -3,12 +3,13 @@ const main = document.querySelector("main");
 // const box = document.getElementById("overlay");
 // const mg = box.querySelector("img");
 // console.log(mg);
-console.log(box);
+// console.log(box);
 // console.log(main);
 // console.log(img);
 
 // ↓要素がない状態
-img.addEventListener('click' ,()=>{
+img.addEventListener('click' ,(evt)=>{
+    console.log(evt.target.src);
     let back = document.createElement("div");
     let pic = document.createElement("picture");
     let mg = document.createElement("img");
