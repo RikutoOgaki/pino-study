f203e9a7e4dec9357e20c9cec11acbcd4216ba53
Rikuto <orikutobasuke@gmail.com>
2022-11-17 22:50:05 +0900

    modal add

--- mod/js/sample09.js
+++ mod/js/sample09.js
@@ -1,25 +1,29 @@
-const img = document.querySelector("img");
+const img = document.querySelectorAll("img");
 const main = document.querySelector("main");
 // const box = document.getElementById("overlay");
 // const mg = box.querySelector("img");
 // console.log(mg);
 // console.log(box);
 // console.log(main);
-// console.log(img);
+console.log(img);
 
 // ↓要素がない状態
-img.addEventListener('click' ,(evt)=>{
-    console.log(evt.target.src);
-    let back = document.createElement("div");
-    let pic = document.createElement("picture");
-    let mg = document.createElement("img");
-    console.log(back);
-    back.classList.add("overlay");
-    back.appendChild(pic);
-    mg.src = "./images/img_modal_kyouto01_l.jpg"
-    pic.appendChild(mg);
-    main.appendChild(back);
+img.forEach((e)=>{
+    e.addEventListener('click' ,(evt)=>{
+        console.log(evt.target.src);
+        let back = document.createElement("div");
+        let pic = document.createElement("picture");
+        let mg = document.createElement("img");
+        // console.log(back);
+        back.classList.add("overlay");
+        back.appendChild(pic);
+        mg.src = "./images/img_modal_kyouto01_l.jpg"
+        pic.appendChild(mg);
+        main.appendChild(back);
+        console.log(back);
+    })
 })
+
 
 // ↓要素がある状態
 // img.addEventListener('click',()=>{
